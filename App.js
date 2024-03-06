6aa4f70ba99ee9768d28aaa599f28333ebd9f764
Sreejith U <126107363+sreejithdev2002@users.noreply.github.com>
2024-03-06 22:37:42 +0530

    Done some code

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const dbConnection = require("./Config/dbConnection");
 const app = express();
+const userRouter = require("./Route/UserRouter");
+const adminRouter = require("./Route/AdminRouter");
 
 //database config
 dbConnection.dbConnect();
@@ -12,3 +14,8 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
+
+
+app.use(cors())
+app.use("/", userRouter)
+app.use("/admin", adminRouter)
